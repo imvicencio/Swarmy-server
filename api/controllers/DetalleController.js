fc35bfbcf604469c1bc5a8bc67acdf653db4c6ca
Ismael Vicencio <imvicencio@live.cl>
2016-07-15 04:23:17 -0400

    add detalle comunidad

---
+++ api/controllers/DetalleController.js
@@ -0,0 +1,11 @@
+/**
+ * DetalleController
+ *
+ * @description :: Server-side logic for managing detalles
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Detalle.js
@@ -0,0 +1,29 @@
+/**
+ * Detalle.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+ module.exports = {
+
+   attributes: {
+     nombre: {
+       type: 'string',
+       required: true
+     },
+     time: {
+       type: 'datetime',
+       required: false
+     },
+     comunidad: {
+       model: 'comunidad',
+       required: true
+     },
+     detalle: {
+       type: 'string',
+       required: true
+     }
+
+   }
+ };
